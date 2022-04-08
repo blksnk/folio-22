b6856370313e6a1faf84854e928c244d9ceb5f50
Jean-Nicolas Veigel <art.jnveigel@gmail.com>
2022-04-08 14:40:40 +0200

    ✨ responsive image

--- src/components/ui/Image.vue
+++ src/components/ui/Image.vue
@@ -1,5 +1,15 @@
 <template>
-  <img :srcset="srcset" :sizes="sizes" :src="defaultSrc" alt="" />
+  <img
+    :class="{
+      ui__image: true,
+      fill: props.fill,
+      }"
+    :srcset="srcset"
+    :sizes="sizes"
+    :src="defaultSrc"
+    alt=""
+    ref="imgRef"
+  />
 </template>
 
 <script setup lang="ts">
@@ -9,7 +19,10 @@ import { computed, ref } from "vue";
 interface ImageProps {
   sources: ImageFormats;
   alt?: string;
+  fill: boolean;
 }
+
+const imgRef = ref<HTMLImageElement>();
 
 const props = defineProps<ImageProps>();
 
@@ -22,14 +35,37 @@ const srcSetDefs = computed(() =>
 );
 
 const srcset = computed(() =>
-  srcSetDefs.value.map(({ width, url }) => `${url} ${width}w`).join(",")
+  [...srcSetDefs.value]
+    .sort((a, b) => a.width - b.width)
+    .map(({ width, url }) => `${url} ${width}w`)
+    .join(",\n")
 );
 
 const sizes = computed(() =>
   srcSetDefs.value
     .map(({ width }, index) =>
-      index ? `(max-width: ${width}) ${width}px` : `${width}px`
+      index < srcSetDefs.value.length - 1
+        ? `(max-width: ${width}px) ${width}px`
+        : `${width}px`
     )
-    .join(",")
+    .join(",\n")
 );
 </script>
+
+<style lang="sass">
+.ui__image
+  image-rendering: crisp-edges
+  image-rendering:  -webkit-optimize-contrast
+  width: 100%
+  height: 100%
+  object-fit: contain
+  object-position: center center
+  user-select: none
+  pointer-events: none
+  transform: translateY(20.5px) scale(1)
+  transition: transform .6s ease-in-out 0s
+
+  &.fill
+    transform: translateY(0px) scale(1.1)
+    transition: transform .6s ease-out 0s
+</style>
